fb68edc201540538ee20696cef79a4e9360f8e3e
mugishakundasarah <mugishakundasarah@gmail.com>
2021-06-10 14:33:47 +0200

    added sign up

---
+++ helperFile.php
@@ -0,0 +1,15 @@
+<?php namespace CodeIgniter;
+
+/**
+ * class Controller
+ */
+class Controller
+{
+    /**
+     * Instance of the main Request object
+     * 
+     * @var HTTP\IncomingRequest
+     */
+    protected $request;
+}
+?>
